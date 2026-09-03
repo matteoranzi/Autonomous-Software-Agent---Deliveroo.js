1b5a8678bbfe2ad42877e7eb971ffea30b7170ea
Matteo Ranzi <matteoranzi2000@gmail.com>
2026-09-03 20:32:00 +0200

    nicer output debug on terminal

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,31 @@ djsClient.onConnect(() => console.log('Connected: ' + djsClient.id));
 djsClient.onDisconnect(() => console.log('Disconnected: ' + djsClient.id));
 
 
+// let belief: Belief;
+// djsClient.onConfig((config) => {
+//     belief = new Belief(adaptConfigPayload(config));
+//
+//     setInterval(() => {
+//         readline.cursorTo(process.stdout, 0, 0);
+//         readline.clearScreenDown(process.stdout);
+//         process.stdout.write(belief.toString());
+//     }, 1000);
+// })
+
 let belief: Belief;
+
+// Render in the terminal's alternate screen buffer, so redraws never touch
+// (or get pushed into) the normal scrollback and scrolling can't reveal stale frames.
+process.stdout.write('\x1b[?1049h\x1b[?25l');
+function restoreTerminal(): void {
+    process.stdout.write('\x1b[?25h\x1b[?1049l');
+}
+process.on('exit', restoreTerminal);
+process.on('SIGINT', () => process.exit());
+
 djsClient.onConfig((config) => {
-    belief = new Belief(adaptConfigPayload(config));
+    gameConfig = adaptConfigPayload(config);
+    belief = new Belief(gameConfig);
 
     setInterval(() => {
         readline.cursorTo(process.stdout, 0, 0);
